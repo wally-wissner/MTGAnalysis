07435cb74312898d44b24c41bae8d02f0ca4c71e
Wally Wissner <Wally.Wissner.MS@gmail.com>
2021-04-12 21:14:21 -0400

    Cleared model pipeline

--- Analysis/Card_Costing/Models/xgb_tfidf/model.py
+++ Analysis/Card_Costing/Models/xgb_tfidf/model.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords as english_stopwords
 from nltk.stem.snowball import SnowballStemmer
 from shutil import rmtree
 from sklearn.compose import ColumnTransformer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.pipeline import FeatureUnion, Pipeline
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.preprocessing import FunctionTransformer
 from sklearn.feature_selection import SelectKBest
+from sklearn.feature_extraction.text import TfidfVectorizer
 from tempfile import mkdtemp
 from xgboost import XGBRegressor
 
@@ -20,59 +21,53 @@ dv = "convertedManaCost"
 ivs = [col for col in df if col not in [dv, "name"]]
 
 
-# NLP helper functions.
-analyzer = CountVectorizer().build_analyzer()
-sw = set(stopwords.words("english")) - {"any", "both", "each", "you", "your"}
-stemmer = SnowballStemmer(language='english')
-def stemming(doc):
-    # Only stem English words.
-    return (stemmer.stem(w) if all(ch.isalpha() or ch == "'" for ch in w) else w for w in analyzer(doc))
+# NLP functions.
+mtg_stopwords = set(english_stopwords.words("english")) - {"any", "both", "each", "you", "your"}
+_english_stemmer = SnowballStemmer(language='english')
+def _analyze_string(doc, ngram_range):
+    words = [
+        _english_stemmer.stem(w) if all(ch.isalpha() or ch == "'" for ch in w) else w
+        for w in doc.split()
+        if w not in mtg_stopwords and _english_stemmer.stem(w) not in mtg_stopwords
+    ]
+    grams = []
+    for n in (ngram_range[0], ngram_range[1] + 1):
+        for word_index in range(len(words) - n):
+            grams.append(" ".join(words[word_index: word_index + n - 1]))
+    return grams
+def mtg_analyzer(doc):
+    return _analyze_string(doc, (1, 3))
 
 
 pipeline_cache = mkdtemp()
-nlp_pipeline = Pipeline(steps=[
-    ("vect", ColumnTransformer([
-        ("tfidf", TfidfVectorizer(
-            analyzer="word",
-            stop_words=sw,
-        ), "text")
-    ])),
-    # ("svd", TruncatedSVD()),
-    # ("best", SelectKBest()),
-])
-other_pipeline = Pipeline(steps=[
-    ("drop_nlp", ColumnTransformer([("dropper", "drop", "text")], remainder='passthrough')),
-])
-pipeline = Pipeline(
-    steps=[
+pipeline = Pipeline(steps=[
+    # ("stem", ColumnTransformer([("stem", FunctionTransformer(func=stem), ["text"])], remainder="passthrough")),
+    ("features", FeatureUnion(transformer_list=[
         (
-            "features",
-            FeatureUnion(
-                transformer_list=[
-                    ("nlp", nlp_pipeline),
-                    ("other", other_pipeline),
-                ]
-            )
+            "nlp",
+            ColumnTransformer([
+                ("tfidf", TfidfVectorizer(analyzer=mtg_analyzer), "text"),
+            ]),
         ),
-        ("xgb", XGBRegressor(booster="gbtree", n_jobs=4)),
-    ],
-    memory=pipeline_cache,
-)
+        (
+            "other",
+            ColumnTransformer([("dropper", "drop", "text")], remainder='passthrough'),
+        ),
+    ])),
+    ("xgb", XGBRegressor()),
+], memory=pipeline_cache)
 rmtree(pipeline_cache)
 
 
 param_grid = {
-    "features__nlp__vect__tfidf__ngram_range": [(1, 3)],
-    "features__nlp__vect__tfidf__max_df": [2 ** i for i in range(-4, 0)],
-    "features__nlp__vect__tfidf__min_df": [2 ** i for i in range(4, 8)],
-    # "features__nlp__vect__tfidf__max_features": [5000],
-    # "features__nlp__best__k": [5000],
-    # "features__nlp__svd__n_components": [100],
+    # "features__nlp__tfidf__ngram_range": [(1, 3)],
+    "features__nlp__tfidf__max_df": [2.0 ** i for i in range(-4, -1)],
+    "features__nlp__tfidf__min_df": [2 ** i for i in range(4, 8)],
     "xgb__n_estimators": [50],
-    "xgb__max_depth": [20],
+    "xgb__max_depth": range(5, 8),
     "xgb__learning_rate": [2 ** i for i in range(-4, 0)],
     "xgb__reg_alpha": [2 ** i for i in range(-3, 0)],
-    "xgb__reg_lambda": [2 ** i for i in range(0, 3)],
+    "xgb__reg_lambda": [2 ** i for i in range(1, 6)],
 }
 
 
